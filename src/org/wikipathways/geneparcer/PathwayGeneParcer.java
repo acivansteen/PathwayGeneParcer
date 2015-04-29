ec2c6a896dfc7456c3ab923a7d132f3f20ee5478
acivansteen <bram_vansteen@hotmail.com>
2015-04-29 16:02:55 +0200

    now shows gene ID

--- src/org/wikipathways/geneparcer/PathwayGeneParcer.java
+++ src/org/wikipathways/geneparcer/PathwayGeneParcer.java
@@ -3,8 +3,10 @@ package org.wikipathways.geneparcer;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.rmi.RemoteException;
+import java.util.List;
 
 import org.bridgedb.DataSource;
+import org.bridgedb.Xref;
 import org.pathvisio.core.model.ConverterException;
 import org.pathvisio.core.model.Pathway;
 import org.pathvisio.wikipathways.webservice.WSPathway;
@@ -28,9 +30,21 @@ public class PathwayGeneParcer {
 		// TODO: 
 		// parse pathway
 	 	WSPathway p = client.getPathway("WP1");
+	 	//makes p the pathway WP1
 	 	Pathway pathway = WikiPathwaysClient.toPathway(p);
 	 	System.out.println(pathway.getMappInfo().getMapInfoName());
-
+	 	List<Xref> list= pathway.getDataNodeXrefs();
+	 	
+	 	for(int i=0; i<list.size();i++){
+	 		System.out.println(list.get(i).getId());
+	 		System.out.println(list.get(i).getDataSource());
+	 	}
+		
+	 	
+	 	
+	 	/*System.out.println(list);
+	 	System.out.println(list.size());
+	 	System.out.println(list.get(2)); */
 	 }
 //	}
 
