621742f023d7d25632933b9efb919a2ae563ec33
acivansteen <bram_vansteen@hotmail.com>
2015-04-30 16:08:28 +0200

    writes data file
    The parser now creates a data file called output.txt in which it writes
    the genes in a tab seperated file.
    pathway name| pathway ID| Gene ID|gene name

--- src/org/wikipathways/geneparcer/PathwayGeneParcer.java
+++ src/org/wikipathways/geneparcer/PathwayGeneParcer.java
@@ -4,11 +4,18 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.rmi.RemoteException;
 import java.util.List;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 
+import org.bridgedb.BridgeDb;
 import org.bridgedb.DataSource;
 import org.bridgedb.Xref;
+import org.pathvisio.core.data.XrefWithSymbol;
 import org.pathvisio.core.model.ConverterException;
 import org.pathvisio.core.model.Pathway;
+import org.pathvisio.core.util.PathwayParser;
 import org.pathvisio.wikipathways.webservice.WSPathway;
 import org.wikipathways.client.WikiPathwaysClient;
 
@@ -19,7 +26,7 @@ import org.wikipathways.client.WikiPathwaysClient;
  */
 public class PathwayGeneParcer {
 
-	public static void main(String[] args) throws MalformedURLException, RemoteException, ConverterException {
+	public static void main(String[] args) throws MalformedURLException, RemoteException, ConverterException, FileNotFoundException, IOException {
 		WikiPathwaysClient client=new WikiPathwaysClient(new URL("http://webservice.wikipathways.org"));
 	 
 //	 WSPathwayInfo [] info = client.listPathways(Organism.HomoSapiens);
@@ -30,22 +37,26 @@ public class PathwayGeneParcer {
 		// TODO: 
 		// parse pathway
 	 	WSPathway p = client.getPathway("WP1");
+
 	 	//makes p the pathway WP1
 	 	Pathway pathway = WikiPathwaysClient.toPathway(p);
 	 	System.out.println(pathway.getMappInfo().getMapInfoName());
+	 	//System.out.println(pathway.);
 	 	List<Xref> list= pathway.getDataNodeXrefs();
 	 	
+	 	PrintWriter writer = new PrintWriter("output.txt","UTF-8");
 	 	for(int i=0; i<list.size();i++){
-	 		System.out.println(list.get(i).getId());
-	 		System.out.println(list.get(i).getDataSource());
+		 	String name=p.getName();
+		 	String id=p.getId();
+	 		String nodeId= list.get(i).getId();
+	 		
+	 		String nodeName = "";
+	 		//cant find anything that works in getting the name, i think i need an xref with symbol 
+	 		//and then display the symbol but i couldnt get it to work
+	 		
+	 		writer.println(name+"\t"+ id+"\t"+ nodeId+"\t"+ nodeName);
+	 		
 	 	}
-		
-	 	
-	 	
-	 	/*System.out.println(list);
-	 	System.out.println(list.size());
-	 	System.out.println(list.get(2)); */
+	 	writer.close();
 	 }
-//	}
-
 }
